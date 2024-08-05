531eddc0266291ac2a112d2b177ec3064fdbbcba
matveymayner <144268527+matvey-mayner@users.noreply.github.com>
2024-08-05 17:05:24 +0300

    Update Exemple App.lua

--- system/bin/Exemple App.lua
+++ system/bin/Exemple App.lua
@@ -2,12 +2,12 @@ local mayner = require("MAYNERAPI")
 local component = require("component")
 local gpu = component.gpu
 local computer
-WinType = "show"
+WinType = "Show"
 
 mayner.Window()
 gpu.set(12, 4, "Exemple")
   mayner.DrawButton(66, 4, 7, 1, "X", 0x000000, 0xFF0000, function()
-    if WinType == "show" then
+    if WinType == "Show" then
       WinType = "hide"
       gpu.setBackground(0x6BC1F7)
       gpu.fill(12, 4, 63, 20, " ")
