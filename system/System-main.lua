89feadf1ac6d85a3147a513b71c364c04d02b4e2
matveymayner <144268527+matvey-mayner@users.noreply.github.com>
2024-08-05 13:52:57 +0300

    Update System-main.lua

--- system/System-main.lua
+++ system/System-main.lua
@@ -42,6 +42,7 @@ drawLoadingBar()
 local fs = require("filesystem")
 local mayner = require("MAYNERAPI")
 local event = require("event")
+local prog = require("programs")
 
 ------------------------------------Main-WorkSpace------------------------------
 drawLoadingBar = nil --Почему мы так решили? потому что мы не крысы и оперативу жрать не будем
@@ -55,15 +56,21 @@ gpu.setForeground(0x000000)
 gpu.fill(1, 1, 80, 1, " ")
 
 local function StartMenu()
-  mayner.DrawButton(1, 2, 7, 1, "Shutdown", 0x000000, 0xFFFFFF, function()
+  mayner.DrawButton(1, 2, 7, 1, "Shutdown   ", 0x000000, 0xFFFFFF, function()
     if StartType == "open" then
         computer.shutdown()
       end
   end)
 
-  mayner.DrawButton(1, 3, 7, 1, "Reboot  ", 0x000000, 0xFFFFFF, function()
+  mayner.DrawButton(1, 3, 7, 1, "Reboot     ", 0x000000, 0xFFFFFF, function()
         if StartType == "open" then
         computer.shutdown(true)
+      end
+  end)
+
+    mayner.DrawButton(1, 4, 7, 1, "AppCenter", 0x000000, 0xFFFFFF, function()
+        if StartType == "open" then
+          assert(prog.execute("/system/bin/AppCenter.lua"))
       end
   end)
 end
@@ -78,7 +85,7 @@ mayner.DrawButton(1, 1, 7, 1, "AmijaOS", 0x000000, 0xFFFFFF, function()
         
         StartType = "close"
         gpu.setBackground(0x6BC1F7)
-        gpu.fill(1, 2, 8, 2, " ")
+        gpu.fill(1, 2, 8, 3, " ")
     elseif StartType == "close" then
           gpu.setBackground(0x6699ff)
           gpu.setForeground(0x000000)
