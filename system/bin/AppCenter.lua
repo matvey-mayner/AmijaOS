d25d35e07e8f0f08694c1e99161c3e31a09a73a5
matveymayner <144268527+matvey-mayner@users.noreply.github.com>
2024-08-05 14:05:49 +0300

    Create AppCenter.lua

---
+++ system/bin/AppCenter.lua
@@ -0,0 +1,11 @@
+local mayner = require("MAYNERAPI")
+local component = require("component")
+local gpu = component.gpu
+local computer
+
+mayner.Window()
+gpu.set(12, 4, "AppCenter")
+  mayner.DrawButton(66, 4, 7, 1, "X", 0x000000, 0xFF0000, function()
+    gpu.setBackground(0x6BC1F7)
+    gpu.fill(12, 4, 63, 20, " ")
+end)
