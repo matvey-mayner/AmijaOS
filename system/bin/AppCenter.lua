0c31e878911e480f06ac158c193e74801f56a59b
matveymayner <144268527+matvey-mayner@users.noreply.github.com>
2024-08-05 23:40:21 +0300

    Update AppCenter.lua

--- system/bin/AppCenter.lua
+++ system/bin/AppCenter.lua
@@ -3,7 +3,10 @@ local component = require("component")
 local gpu = component.gpu
 local computer = require("computer")
 
-
+gpu.setBackground(0x707070)
+gpu.fill(1, 2, 30, 25, " ")
+gpu.setBackground(0x707070)
+gpu.fill(31, 2, 50, 25, " ")
 
 mayner.DrawButton(30, 8, 7, 3, "FileManager", 0x000000, 0xFFFFFF, function()
       assert(prog.execute("/system/bin/filemanager.lua"))
