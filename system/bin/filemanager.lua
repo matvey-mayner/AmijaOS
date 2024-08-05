8b062b5a7a40ed0ef5d78c99c7727605ab60c54f
matveymayner <144268527+matvey-mayner@users.noreply.github.com>
2024-08-05 23:23:27 +0300

    Update filemanager.lua

--- system/bin/filemanager.lua
+++ system/bin/filemanager.lua
@@ -1,4 +1,26 @@
+local component = require("component")
+local fs = require("filesystem")
+local event = require("event")
+local gpu = component.gpu
 local mayner = require("MAYNERAPI")
+-- Переменная для строки, на которой будет происходить вывод
+y = 5
 
-mayner.Window()
-gpu.set(10, 4, "FileManager")
+gpu.setBackground(0x707070)
+gpu.fill(1, 2, 30, 25, " ")
+gpu.setBackground(0xFFFFFF)
+gpu.fill(31, 2, 50, 25, " ")
+
+-- Получаем список файлов и директорий
+local files = fs.list("/")
+
+--[[
+gpu.setBackground(0x00FF00)
+gpu.fill(1, 2, 80, 25, " ")
+]]--
+  
+-- Выводим содержимое на экран
+for i, name in ipairs(files) do
+    gpu.set(14, y, name)  -- Выводим имя файла или директории
+    y = y + 1            -- Переходим на следующую строку
+end
