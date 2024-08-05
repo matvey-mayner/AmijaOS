cccfbb8a56972c7b99d0ffc240d1d480ccb8240e
matveymayner <144268527+matvey-mayner@users.noreply.github.com>
2024-08-05 14:06:08 +0300

    Update Exemple App.lua

--- system/bin/Exemple App.lua
+++ system/bin/Exemple App.lua
@@ -1,10 +1,11 @@
 local mayner = require("MAYNERAPI")
 local component = require("component")
 local gpu = component.gpu
+local computer
 
 mayner.Window()
-gpu.set(10, 4, "Exemple")
-  mayner.DrawButton(63, 4, 7, 1, "X", 0x000000, 0xFF0000, function()
+gpu.set(12, 4, "Exemple")
+  mayner.DrawButton(66, 4, 7, 1, "X", 0x000000, 0xFF0000, function()
     gpu.setBackground(0x6BC1F7)
-    gpu.fill(10, 4, 63, 20, " ")
+    gpu.fill(12, 4, 63, 20, " ")
 end)
