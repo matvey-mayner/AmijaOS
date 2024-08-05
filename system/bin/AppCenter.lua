45a02fa48348f8005b682fe33bba0bf2853753b2
matveymayner <144268527+matvey-mayner@users.noreply.github.com>
2024-08-05 18:17:41 +0300

    Update AppCenter.lua

--- system/bin/AppCenter.lua
+++ system/bin/AppCenter.lua
@@ -2,11 +2,18 @@ local mayner = require("MAYNERAPI")
 local component = require("component")
 local gpu = component.gpu
 local computer
+WinType = "show"
 
 mayner.Window()
 gpu.set(12, 4, "AppCenter")
   mayner.DrawButton(66, 4, 7, 1, "X", 0x000000, 0xFF0000, function()
-    gpu.setBackground(0x6BC1F7)
-    gpu.fill(12, 4, 63, 20, " ")
+    if WinType == "show" then
+      WinType = "hide"
+      gpu.setBackground(0x6BC1F7)
+      gpu.fill(12, 4, 63, 20, " ")
+    end
 end)
 
+mayner.DrawButton(30, 8, 7, 3, "FileManager", 0x000000, 0xFFFFFF, function()
+      assert(prog.execute("/system/bin/filemanager.lua"))
+end)  
