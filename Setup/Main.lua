c768eabc83fe0276cdcdd5c2fbb2c33109b167dc
matveymayner <144268527+matvey-mayner@users.noreply.github.com>
2024-08-13 10:48:45 +0300

    Update Main.lua

--- Setup/Main.lua
+++ Setup/Main.lua
@@ -13,7 +13,7 @@ gpu.fill(1, 1, 80, 25, " ")
 
 mayner.Window()
 
-buttons1 = "Show"
+buttons1 = "Show" --Ох всякие странные переменый, и сюда пришли!
 
 gpu.set(12, 4, "Amija OS Setup")
 
@@ -25,12 +25,18 @@ end)
     gpu.setForeground(0x000000)
     gpu.fill(25, 6, 37, 15, " ")
 
-    gpu.set(25, 6, "You agree to the GNU GPL V3 Licenses")
+---------------Это То что будет выводиться на тёмном прямоугольничке
+
+    gpu.set(25, 6, "You agree with the GNU GPL V3")
     gpu.set(25, 7, "And The System Rules")
     gpu.set(25, 8, "When you install The System (AmijaOS)")
     gpu.set(25, 9, "!!!YOUR ALL DATA WILL BE ERASED!!!")
     gpu.set(25, 10, "!!!AND WHEN YOU INSTALLING AMIJAOS!!!")
     gpu.set(25, 11, "!!!YOUR EEPROM WILL BE REWRITED!!!")
+    gpu.set(25, 12, "And you not have root access")
+    gpu.set(25, 13, "To edit The system!")
+    gpu.set(25, 14, "But Орешки тупитупи cant delete") -- Ну Это Правда!
+    gpu.set(25, 15, "System!")
 
 mayner.DrawButton(69, 23, 6, 1, "Accept", 0xFFFFFF, 0x4b4b4b, function()
   if buttons1 == "Show" then
