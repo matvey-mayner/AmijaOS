cc095fdb052a0fdf0a2309b44baecdb9a1beb632
matveymayner <144268527+matvey-mayner@users.noreply.github.com>
2024-08-17 22:36:44 +0300

    Update System-main.lua

--- system/System-main.lua
+++ system/System-main.lua
@@ -7,39 +7,20 @@ AppOpen = nil
 
 gpu.setResolution(80, 25)
 
-local function drawLoadingBar()
-  local barWidth = 50
-  local barHeight = 1
-  local barX = math.floor((80 - barWidth) / 2)
-  local barY = 13
-
-  gpu.setForeground(0xFFFFFF)
-  gpu.setBackground(0x000000)
-  gpu.fill(barX, barY, barWidth, barHeight, " ")
-
-  local progress = 0
-  while progress <= barWidth do
-    gpu.setForeground(0xFFFFFF)
-    gpu.setBackground(0xFFFFFF)
-    gpu.fill(barX, barY, progress, barHeight, " ")
-    gpu.setForeground(0x000000)
-    gpu.setBackground(0x000000)
-    gpu.fill(barX + progress, barY, 1, barHeight, " ")
-
-    os.sleep(0.05)
-    progress = progress + 1
-  end
-end
-
+gpu.setBackground(0x000000)
 gpu.setForeground(0xFFFFFF)
-gpu.setBackground(0xbb6058)
-gpu.fill(1, 1, 80, 25, " ")
+gpu.set(37, 9, "AmijaOS")
+gpu.set(37, 11, "Kernel Initialized!")
+os.sleep(0.12)
+gpu.setBackground(0x000000)
+gpu.fill(37, 11, 19, 1, " ")
+gpu.set(37, 11, "API Initialized!")
+os.sleep(0.12)
+gpu.setBackground(0x000000)
+gpu.fill(37, 11, 16, 1, " ")
+gpu.set(37, 11, "The System Is Initialized...")
+os.sleep(0.12)
 
-gpu.setBackground(0xbb6058)
-gpu.setForeground(0xFFFFFF)
-gpu.set(37, 12, "AmijaOS")
-
-drawLoadingBar()
 local fs = require("filesystem")
 local mayner = require("MAYNERAPI")
 local event = require("event")
