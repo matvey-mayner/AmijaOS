19db6b9b33ce09fe190fbd989f95efda0f99e0d2
DanXvoIsMe <157852596+DanXvoIsMe@users.noreply.github.com>
2024-08-18 19:34:33 +0200

    Update MAYNERAPI.lua

--- system/core/lib/MAYNERAPI.lua
+++ system/core/lib/MAYNERAPI.lua
@@ -75,7 +75,7 @@ function MAYNERAPI.ScreenScale(SCX, SCY)
     gpu.setResolution(#SCX, #SCY)
 end
 
-function MAYNERAPI.downloadfilefromurl(url, dist)
+function MAYNERAPI.DownloadFileFromUrl(url, dist)
     local handle, data, result, reason = internet.request(url), ""
     if handle then
         local file, fileError = io.open(dist, "wb") -- Open the file in binary write mode
@@ -103,6 +103,27 @@ function MAYNERAPI.downloadfilefromurl(url, dist)
     end
 end
 
+local function GetDataFromUrl(url)
+    local handle, data, result, reason = internet.request(url), ""
+    if handle then
+        while true do
+            result, reason = handle.read(math.huge) 
+            if result then
+                data = data .. result
+            else
+                handle.close()
+                
+                if reason then
+                    return nil, reason
+                else
+                    return data
+                end
+            end
+        end
+    else
+        return nil, "unvalid address"
+    end
+end
 
 --[[
 function MAYNERAPI.SYSRM()
