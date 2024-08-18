85a7c08c4e87fd149bcbec50c31822bbd8e948de
DanXvoIsMe <157852596+DanXvoIsMe@users.noreply.github.com>
2024-08-18 19:30:51 +0200

    Update MAYNERAPI.lua

--- system/core/lib/MAYNERAPI.lua
+++ system/core/lib/MAYNERAPI.lua
@@ -6,6 +6,7 @@ local computer = require("computer")
 local fs = require("filesystem")
 local internet = require("internet")
 local os = require("os")
+local internet = component.proxy(component.list("internet")() or "")
 
 MAYNERAPI = {}
 
@@ -73,6 +74,35 @@ end
 function MAYNERAPI.ScreenScale(SCX, SCY)
     gpu.setResolution(#SCX, #SCY)
 end
+
+function MAYNERAPI.downloadfilefromurl(url, dist)
+    local handle, data, result, reason = internet.request(url), ""
+    if handle then
+        local file, fileError = io.open(dist, "wb") -- Open the file in binary write mode
+        if not file then
+            return nil, "Could not open file: " .. fileError
+        end
+        
+        while true do
+            result, reason = handle.read(math.huge)
+            if result then
+                file:write(result) -- Write the result to the file
+            else
+                handle.close()
+                file:close()
+                
+                if reason then
+                    return nil, reason
+                else
+                    return true -- Return true to indicate successful download
+                end
+            end
+        end
+    else
+        return nil, "Invalid address"
+    end
+end
+
 
 --[[
 function MAYNERAPI.SYSRM()
