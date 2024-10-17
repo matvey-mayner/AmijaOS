1c27417f6d7038f5e8a9bd23fb6591161316d62b
gamma63 <webe-not-inc@proton.me>
2024-10-17 20:55:55 +0300

    Update System-main.lua

--- system/System-main.lua
+++ system/System-main.lua
@@ -2,7 +2,7 @@ local component = require("component")
 local gpu = component.gpu
 local computer = require("computer")
 
-local version = "1.2.2"
+local version = "1.1.1"
 
 StartType = "close"
 AppOpen = nil
